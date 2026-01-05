789e5be10ae20b8d8075e33f0439a0e2e99521c3
okkop <okkop@invalid.com>
2026-01-05 19:16:28 +0200

    Refactor all :D

--- main.py
+++ main.py
@@ -1,85 +1,67 @@
+from unittest.mock import MagicMock
 from gpiozero import Button
-import asyncio
 import telegram
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
 from time import localtime, strftime, sleep
 
-tokenfile = ""
+APP_VERSION = "0.0.1"
+
+tokenfile = "tg_token.dev.txt"
 channel = ""
-kytkin = Button(23)
+kytkin =  MagicMock() #Button(23)
+kytkin.is_pressed = False
 
-with open(tokenfile, 'r') as file:
-    token = file.readline().strip()
 
-def print_help(update: telegram.Update, context: CallbackContext):
-    update.message.reply_text("/help - print this message\n/iltaa - show the clubroom door status")
 
-def status(update: telegram.Update, context: CallbackContext):
+
+def get_bot_token() -> str:
+    
+    # TODO: use environment variable
+    def get_token_file_name() -> str:
+        return "tg_token.dev.txt"
+    
+    with open(get_token_file_name(), 'r') as file:
+        token = file.readline().strip()
+        if not token:
+            raise ValueError("Token was read but is empty")
+        return token
+
+
+async def cmd_help(update: telegram.Update, ctx: CallbackContext) -> None:
+    print("help command called")
+    await update.message.reply_text("/help - print this message\n/iltaa - show the clubroom door status")
+
+
+async def cmd_status(update: telegram.Update, context: CallbackContext):
     global kytkin
 
     if kytkin.is_pressed:
-        update.message.reply_text("Spinni is closed.")
+        await update.message.reply_text("Spinni is closed.")
     else:
-        update.message.reply_text("Spinni is open!")
+        await update.message.reply_text("Spinni is open!")
 
-async def remove(msg_id):
-    global token
-    bot = telegram.Bot(token)
-    global channel
-
-    try:
-        bot.delete_message(channel, msg_id)
-    except telegram.error.NetworkError:
-        pass
-
-async def send(status):
-    global token
-    bot = telegram.Bot(token)
-    global channel
-
-    if status:
-        door_status = "closed."
-    else:
-        door_status = "open!"
-    
-    timestamp = f"\n({strftime('%F %R', localtime())})"
-    message = f"Spinni is {door_status}{timestamp}"
-    
-    try:
-        sent_message = bot.send_message(channel, message)
-        message_id = sent_message.message_id
-        return message_id
-    except telegram.error.NetworkError:
-        pass
     
 
 def main():
-    global token
-    
-    global kytkin
-    message_id = None
-    edellinen_tila = kytkin.is_pressed
-    edellinen_id = None
-    updater = telegram.ext.Updater(token)
-    
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("help", print_help))
-    dispatcher.add_handler(CommandHandler("iltaa", status))
-    updater.start_polling()
-    
-    while True:
-        nykyinen_tila = kytkin.is_pressed
-        if nykyinen_tila != edellinen_tila:
-        
-            if not edellinen_id == message_id:
-                asyncio.run(remove(message_id))
-                edellinen_id = message_id
-                sleep(0.1)
 
-            message_id = asyncio.run(send(nykyinen_tila))
-            edellinen_tila = nykyinen_tila
-        
-        sleep(0.9)
+    token: str = get_bot_token()
+
+    COMMANDS = (
+        CommandHandler("help", cmd_help),
+        CommandHandler("iltaa", cmd_status)
+    )
+
+    app = Application.builder().token(token).build()
+    print(f"Bot initialized")
+
+
+    for cmd_handler in COMMANDS:
+        print(f"Adding command handler: {cmd_handler}")
+        app.add_handler(cmd_handler)
+    
+
+    print("Running...")
+    app.run_polling()
         
 
 if __name__ == "__main__":
