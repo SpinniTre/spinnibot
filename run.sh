1a559a9fb892d4d6edd05657bdcf739914eb169b
okkop <okkop@invalid.com>
2026-01-12 18:55:38 +0200

    Improve run and update bash scipts

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python3 --version
+python3 main.py
--- update.sh
+++ update.sh
@@ -1,23 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-## Work in progress
+set -e  # exit on error
 
-SRC_BRANCH = "master"
+SRC_BRANCH="sandbox"   # or "master"
+VENV_DIR="myvenv"
 
 echo "Running update script"
 
-echo "Killing process first"
 
-# Deactivate current virtualenv
-deactivate
 
-echo "Fetch code from remote branch $SRC_BRANCH"
-echo git remote -v
+echo "Fetching code from remote branch '$SRC_BRANCH'"
 
-git checkout master
+git fetch
+git checkout "$SRC_BRANCH"
+git pull origin "$SRC_BRANCH"
+
+BEFORE=$(git rev-parse HEAD)
 git pull
+AFTER=$(git rev-parse HEAD)
 
-echo "Restarting"
+# Check if code has changed
+if [[ "$BEFORE" != "$AFTER" ]]; then
+    
+    # Deactivate current virtualenv if active
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        echo "Deactivating current virtualenv"
+        deactivate
+    fi
 
+    echo "Changes detected ($BEFORE <> $AFTER)"
+    echo "Restarting"
+
+    # Activate virtualenv
+    if [[ ! -d "$VENV_DIR" ]]; then
+        echo "Virtualenv not found: $VENV_DIR"
+        exit 1
+    fi
+
+    echo "venv activated"
+    source "./$VENV_DIR/bin/activate"
+
+    # Install/update deps
+    if [[ -f requirements.txt ]]; then
+        pip install -r requirements.txt
+    fi
+
+else
+    echo "No changes in branch. Done."
+fi
+
+# Run
+
+echo "Run bot"
 bash ./run.sh
-
